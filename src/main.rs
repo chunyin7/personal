2ff78cdff936aab523910735f1bba24ccb22b4b6
milesng <miles.c.y.ng@gmail.com>
2025-12-02 23:20:34 +1100

    basic styling setup

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,22 @@
-use axum::{Router, routing::get};
+use askama::Template;
+use axum::{Router, response::Html, routing::get};
+use tower_http::services::ServeDir;
+
+#[derive(Template)]
+#[template(path = "index.html")]
+struct IndexTemplate;
+
+async fn index() -> Html<String> {
+    let template = IndexTemplate {};
+    Html(template.render().unwrap())
+}
 
 #[tokio::main]
 async fn main() {
-    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+    let app = Router::new()
+        .route("/", get(index))
+        .nest_service("/static", ServeDir::new("static"))
+        .nest_service("/assets", ServeDir::new("assets"));
 
     let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await.unwrap();
     axum::serve(listener, app).await.unwrap();
