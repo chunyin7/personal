37b3069ef7a42216617adcae6cbde83985eb9b38
milesng <miles.c.y.ng@gmail.com>
2025-12-07 21:06:08 +1100

    fix home page

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,21 @@
 use askama::Template;
 use axum::{Router, response::Html, routing::get};
+use chrono::{DateTime, TimeZone, Utc};
 use tower_http::services::ServeDir;
 
 #[derive(Template)]
 #[template(path = "index.html")]
-struct IndexTemplate;
+struct IndexTemplate {
+    age: u32,
+}
 
 async fn index() -> Html<String> {
-    let template = IndexTemplate {};
+    let template = IndexTemplate {
+        age: Utc::now()
+            .years_since(Utc.with_ymd_and_hms(2005, 4, 24, 0, 0, 0).unwrap())
+            .unwrap()
+            .into(),
+    };
     Html(template.render().unwrap())
 }
 
